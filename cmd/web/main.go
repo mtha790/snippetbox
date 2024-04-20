1d5f7c6949803d642a2908335904446fba127e9a
mtha790 <>
2024-04-20 10:42:14 +0200

    add routes method to application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,22 +14,10 @@ type application struct {
 func main() {
 	addr := flag.String("addr", ":4000", "Http Network Address")
 	flag.Parse()
-
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-
 	app := &application{logger: logger}
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux := http.NewServeMux()
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
