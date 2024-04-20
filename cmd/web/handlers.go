7efc6ebd78c27faf28bea044f63988f9b5182ef9
mtha790 <>
2024-04-20 18:16:29 +0200

    rename serveError into serverError

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,7 +14,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.serveError(w, r, err)
+		app.serverError(w, r, err)
 		return
 	}
 
@@ -55,7 +55,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		if errors.Is(err, models.ErrNoRecord) {
 			http.NotFound(w, r)
 		} else {
-			app.serveError(w, r, err)
+			app.serverError(w, r, err)
 		}
 		return
 	}
@@ -73,7 +73,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
-		app.serveError(w, r, err)
+		app.serverError(w, r, err)
 		return
 	}
 
--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,7 +5,7 @@ import (
 	"runtime/debug"
 )
 
-func (app *application) serveError(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
 		method = r.Method
 		uri    = r.URL.RequestURI()
