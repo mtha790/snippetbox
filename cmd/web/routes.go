9a6392b93b7e391ff0f4d65db292a863f4b32435
mtha790 <>
2024-04-21 16:21:52 +0200

    add panic recovery middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,8 @@ package main
 
 import "net/http"
 
+type Middleware func(http.Handler) http.Handler
+
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -10,5 +12,17 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	return app.logRequest(commonHeaders(mux))
+
+	middlewares := []Middleware{
+		commonHeaders,
+		app.logRequest,
+		app.recoverPanic,
+	}
+
+	var handler http.Handler = mux
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+
+	return handler
 }
