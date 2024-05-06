4b83dd13cd6330cbd51eab7f606fe8243a0c2f21
mtha790 <>
2024-05-06 19:59:52 +0200

    improve authentication

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
@@ -9,7 +10,10 @@ import (
 
 func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		w.Header().Set(
+			"Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com",
+		)
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
@@ -64,17 +68,61 @@ func noSurf(next http.Handler) http.Handler {
 	return csrfHandler
 }
 
+// Set the isAuthenticatedContextKey to true in the request context if the user is authenticated and go to the next handler.
+// Return an error if a bad id is provided.
+// Otherwise if not id is provided pass immediatly to the next handler
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := app.sessionManager.GetInt(r.Context(), "authenticatedUserID")
+		if id == 0 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		exists, err := app.users.Exists(id)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		if exists {
+			ctx := context.WithValue(r.Context(), isAuthenticatedContextKey, true)
+			r = r.WithContext(ctx)
+		}
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+type Middleware func(next http.Handler) http.Handler
+
+func chain(middlewares ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for _, middleware := range middlewares {
+			next = middleware(next)
+		}
+		return next
+	}
+}
+
 func (app *application) dynamic(next http.Handler) http.Handler {
-	return app.sessionManager.LoadAndSave(
-		noSurf(next))
+	return chain(
+		app.authenticate,
+		noSurf,
+		app.sessionManager.LoadAndSave,
+	)(next)
 }
 
 func (app *application) dynamicWithAuth(next http.Handler) http.Handler {
-	return app.dynamic(app.requireAuthentication(next))
+	return chain(
+		app.requireAuthentication,
+		app.dynamic,
+	)(next)
 }
 
 func (app *application) standard(next http.Handler) http.Handler {
-	return app.recoverPanic(
-		app.logRequest(
-			commonHeaders(next)))
+	return chain(
+		commonHeaders,
+		app.logRequest,
+		app.recoverPanic,
+	)(next)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,41 +11,19 @@ func (app *application) routes() http.Handler {
 			http.FileServer(http.Dir("./ui/static/")),
 		),
 	)
-	mux.Handle(
-		"GET /{$}",
-		app.dynamic(http.HandlerFunc(app.home)),
-	)
-	mux.Handle(
-		"GET /snippet/view/{id}",
-		app.dynamic(http.HandlerFunc(app.snippetView)),
-	)
-	mux.Handle(
-		"GET /user/signup",
-		app.dynamic(http.HandlerFunc(app.userSignup)),
-	)
-	mux.Handle(
-		"POST /user/signup",
-		app.dynamic(http.HandlerFunc(app.userSignupPost)),
-	)
-	mux.Handle(
-		"GET /user/login",
-		app.dynamic(http.HandlerFunc(app.userLogin)),
-	)
-	mux.Handle(
-		"POST /user/login",
-		app.dynamic(http.HandlerFunc(app.userLoginPost)),
-	)
-	mux.Handle(
-		"GET /snippet/create",
-		app.dynamicWithAuth(http.HandlerFunc(app.snippetCreate)),
-	)
-	mux.Handle(
-		"POST /snippet/create",
-		app.dynamicWithAuth(http.HandlerFunc(app.snippetCreatePost)),
-	)
-	mux.Handle(
-		"POST /user/logout",
-		app.dynamicWithAuth(http.HandlerFunc(app.userLogoutPost)),
-	)
+
+	// Dynamic requests
+	mux.Handle("GET /{$}", app.dynamic(http.HandlerFunc(app.home)))
+	mux.Handle("GET /snippet/view/{id}", app.dynamic(http.HandlerFunc(app.snippetView)))
+	mux.Handle("GET /user/signup", app.dynamic(http.HandlerFunc(app.userSignup)))
+	mux.Handle("POST /user/signup", app.dynamic(http.HandlerFunc(app.userSignupPost)))
+	mux.Handle("GET /user/login", app.dynamic(http.HandlerFunc(app.userLogin)))
+	mux.Handle("POST /user/login", app.dynamic(http.HandlerFunc(app.userLoginPost)))
+
+	// Dynamic requests that require authentication
+	mux.Handle("GET /snippet/create", app.dynamicWithAuth(http.HandlerFunc(app.snippetCreate)))
+	mux.Handle("POST /snippet/create", app.dynamicWithAuth(http.HandlerFunc(app.snippetCreatePost)))
+	mux.Handle("POST /user/logout", app.dynamicWithAuth(http.HandlerFunc(app.userLogoutPost)))
+
 	return app.standard(http.Handler(mux))
 }
